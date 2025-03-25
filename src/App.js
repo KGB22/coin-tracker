d990760a5b552762d863f8d13f51f6c2366e8d60
KGB22 <gggggggyuu9@gmail.com>
2025-03-25 19:08:30 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSelect = (event) => {
+    const coinName = event.target.value;
+    const coin = coins.find((c) => c.name === coinName);
+    setSelectedCoin(coin);
+  };
+
+  function reset() {
+    setAmount("");
+  }
+
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.box}>
+      <div className={styles.title}>
+        Coin Tracker
+        {!loading && (
+          <>
+            {" ("}
+            <span>{coins.length}</span>
+            {")"}
+          </>
+        )}
+      </div>
+
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <div className={styles.select_box}>
+            <select onChange={handleSelect} defaultValue="">
+              <option value="" disabled>
+                Select Coin!
+              </option>
+              {coins.map((coin) => (
+                <option key={coin.rank} value={coin.name}>
+                  {coin.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.input_box}>
+            <input
+              type="number"
+              id="dollars"
+              placeholder="Dollars"
+              value={amount}
+              onChange={onChange}
+            />
+            <label htmlFor="dollars">Dollars</label>
+          </div>{" "}
+          {selectedCoin !== "" ? (
+            <div className={styles.input_box}>
+              <input
+                id="btc"
+                placeholder={selectedCoin.symbol}
+                value={(amount / selectedCoin.quotes.USD.price).toFixed(2)}
+                disabled
+              />
+              <label htmlFor="btc">{selectedCoin.symbol}</label>
+            </div>
+          ) : null}
+          <div className={styles.button_box}>
+            <Button reset={reset} text="Reset" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
